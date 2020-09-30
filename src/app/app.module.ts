6d00cfb7dbc484f8662c4ab72f33c6e8da9fcbab
ashish choksi <ashishchoksi84@gmail.com>
2020-09-30 11:29:34 +0530

    angular lab assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { FeedBackComponent } from './feed-back/feed-back.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginDetailComponent } from './login-detail/login-detail.component';
+import { RegisterDetailComponent } from './register-detail/register-detail.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDetailsComponent,
+    FeedBackComponent,
+    AboutUsComponent,
+    HomePageComponent,
+    LoginDetailComponent,
+    RegisterDetailComponent
   ],
   imports: [
     BrowserModule,
